a4a669885f4893aac4bacff7c71eeb4b5f0a7acb
relf <remi.lafage@onera.fr>
2016-11-14 14:28:14 +0100

    take into account bootstrap 4 and improve mockup

--- app/controllers/api/v1/studies_controller.rb
+++ app/controllers/api/v1/studies_controller.rb
@@ -1,8 +1,23 @@
 class Api::V1::StudiesController < Api::ApiController 
 
+  # GET /studies
   def index
     @studies = Study.all
     respond_with @studies
   end
- 
+
+  # POST /studies
+  def create
+    @study = Study.create(study_params)
+    respond_with @study
+  end
+
+  private
+
+  def study_params
+    # FIXME: should validate the tree and conns jsons
+    # Can not use string parameters as is for tree structure is recursive
+    params.require(:study).permit!
+  end
+  
 end
--- app/models/study.rb
+++ app/models/study.rb
@@ -4,8 +4,10 @@ class Study < ApplicationRecord
   resourcify
 
   belongs_to :project, :dependent => :destroy
-  has_many :run
+  has_many :runs
 
   serialize :tree_json, JSON
   serialize :conns_json, JSON
+
+  validates :project, presence: true 
 end
