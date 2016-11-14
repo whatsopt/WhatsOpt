3721dfaa29cb8c209baddf88197357d3b838bf44
relf <remi.lafage@onera.fr>
2016-11-14 14:25:32 +0100

    initialize tests

--- test/models/study_test.rb
+++ test/models/study_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class StudyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save a study without a project" do
+    study = Study.new
+    assert_not study.save
+  end
 end
