cf1823c8c0c3e734f1586d7213fdfd52854c9ca1
relf <remi.lafage@onera.fr>
2016-12-02 15:28:54 +0100

    mix ldap and database auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
   rolify
   
-  devise :database_authenticatable, :trackable, :validatable, :timeoutable
+  devise :ldap_authenticatable, :database_authenticatable, :trackable, :validatable, :timeoutable
 
   before_create :generate_api_key
+
+  # Used to create user on first LDAP authentication 
+  def ldap_before_save
+    self.email = Devise::LDAP::Adapter.get_ldap_param(self.login, "mail").first
+  end
 
   private
   
