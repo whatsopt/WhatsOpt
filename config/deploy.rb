7efcd3addb185f0114adb3a0a5307c2fd66435b1
relf <remi.lafage@onera.fr>
2017-01-16 14:38:41 +0100

    fixing...

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :deploy_to, "/var/www/html/rails/whatsopt"
 
 set :log_level, :info
 
-#set :linked_files, %w{config/database.yml config/secrets.yml}
+# set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp upload vendor/bundle public/system}
 
 SSHKit.config.command_map[:rake]  = "bundle exec rake" 
