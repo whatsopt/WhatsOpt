5af78df2f87309a86099ef721f720b77b7ba87a0
Rémi Lafage <remi.lafage@onera.fr>
2017-04-17 23:44:10 +0200

    work on excel importer

--- lib/whats_opt/excel_mda_importer.rb
+++ lib/whats_opt/excel_mda_importer.rb
@@ -2,24 +2,45 @@ module WhatsOpt
 
   class ExcelMdaImporter
     
+    attr_reader :line_count
+    
+    FIRST_LINE_NB = 15
+    
     def initialize(filename)
       @workbook = RubyXL::Parser.parse(filename)
       @worksheet = @workbook[0]
-      @workdata = @worksheet[15..248].map{|row| row && row[4..16]} 
+      workprobe = @worksheet[FIRST_LINE_NB..1024]
+      @line_count = 0
+      while workprobe[@line_count][5]
+        @line_count += 1
+      end 
+      @worksheet = @worksheet[FIRST_LINE_NB...(FIRST_LINE_NB+@line_count)]
+      @workdata = @worksheet.map{|row| row && row[4..16]}
       @workdata.compact!
     end
-
+    
     def get_disciplines
       disciplines = @workdata.map{|row| row && row[1].value}
-      disciplines.uniq!.compact!
+      disciplines = disciplines.uniq.compact
       return disciplines
     end
     
     def get_variables(discipline)
-      #variables = @workdata.select{|row| row && row[1].value == discipline}
-      #variables = .map{|row| row && row[12].value}
-      #variables.uniq!.compact!
-      #return variables
+      rows = @workdata.select{|row| row && row[1].value == discipline}
+      rows.compact!
+      variables = []
+      rows.each do |row|
+        variables.append({name: row[12].value, type: row[3].value, unit: row[5].value})
+      end
+      return variables
+    end
+    
+    def get_connections()
+      flows = @workdata.map{|row| row && row[6..12]}
+      flows.each do |f|
+        puts f[0].value
+      end
+      return {}
     end
 
   end
--- test/lib/excel_mda_importer_test.rb
+++ test/lib/excel_mda_importer_test.rb
@@ -4,20 +4,34 @@ require 'whats_opt/excel_mda_importer'
 class ExcelMdaImporterTest < ActiveSupport::TestCase
 
   def setup
-    @emi = WhatsOpt::ExcelMdaImporter.new(sample_file("excel_mda_sample.xlsm"))  
+    @emi = WhatsOpt::ExcelMdaImporter.new(sample_file("excel_mda_simple_sample.xlsm"))  
   end
 
-  test "should read disciplines" do
-    assert_equal ['Geometry', 'Propulsion', 'Aerodynamics', 'MCI', 'Mission',
-                  'Structure', 'Control'], @emi.get_disciplines
+  test "should get excel line count" do
+    assert_equal 8, @emi.line_count
+  end
+  
+  test "should import disciplines" do
+    assert_equal ['Geometry', 'Control'], @emi.get_disciplines
   end
 
-  test "should read variables" do
-    assert_equal ['flight_qualities_point_table', 'nb_rctrl_surface', 
-                  'ctrl_surface_chord', 'ctrl_surface_root_pos_y',
-                  'ctrl_surface_tip_pos_y','ctrl_surface_role',
-                  'rudder_chord','rudder_role','lambda_table'], 
-                 @emi.get_variables('Control')
+  test "should import variables of given discipline" do
+    assert_equal [{name: 'flight_qualities_point', type: 'table', unit: '(-)'}, 
+                  {name: 'nb_rctrl_surface', type: 'scalaire', unit: '(-)'}, 
+                  {name: 'ctrl_surface_chord', type: 'table', unit: 'm'}, 
+                  {name: 'lambda_table', type: 'table', unit: '(-)'}], 
+                  @emi.get_variables('Control')
+  end
+  
+  test "should import connections" do
+    skip('not yet implemented')
+    assert_equal({
+      Y02: ['nb_rctrl_surface', 'ctrl_surface_chord', 'ctrl_surface_root_pos_y', 'ctrl_surface_tip_pos_y', 'rudder_chord'], 
+      Y03: ['nb_rctrl_surface', 'ctrl_surface_chord', 'ctrl_surface_root_pos_y', 'ctrl_surface_tip_pos_y', 'rudder_chord'], 
+       X5: ['flight_qualities_point_table', 'ctrl_surface_role'], 
+       X0: ['nb_rctrl_surface', 'ctrl_surface_chord', 'rudder_chord'],
+      Y5x: ['lambda_table']
+    }, @emi.get_connections('Control'))
   end
   
   test "should generate apologize html when bad file format" do
