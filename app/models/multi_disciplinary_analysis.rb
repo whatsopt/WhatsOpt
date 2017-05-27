5e00d8df5173e58a7642d1ecedd2c22534d75083
Rémi Lafage <remi.lafage@onera.fr>
2017-05-27 22:10:47 +0200

    refactor

--- app/models/multi_disciplinary_analysis.rb
+++ app/models/multi_disciplinary_analysis.rb
@@ -25,15 +25,13 @@ class MultiDisciplinaryAnalysis < ApplicationRecord
   end
 
   def build_nodes
-    nodes = disciplines.map {|d| { id: "#{d.id}", 
-                                   type: "analysis", 
-                                   name: d.name } }
+    nodes = disciplines.map {|d| { id: "#{d.id}", type: "analysis", name: d.name } }
     return nodes
   end
 
   def build_edges
     edges = []
-    all_connections = Set.new
+    @all_connections = Set.new
 
     # connections
     disciplines.each do |d_from|
@@ -42,42 +40,39 @@ class MultiDisciplinaryAnalysis < ApplicationRecord
         next if d_to == d_from
         inputs = d_to.input_variables
         connections = outputs.map(&:name) & inputs.map(&:name)
-        all_connections.merge(connections)
+        @all_connections.merge(connections)
         unless connections.empty?
-          edges << { from: "#{d_from.id}", to: "#{d_to.id}", 
-                     name: connections.join(",") }
+          edges << { from: "#{d_from.id}", to: "#{d_to.id}", name: connections.join(",") }
         end
       end
     end
 
     # pendings
     disciplines.each do |d|
-      pendings = []
-      d.input_variables.each do |v|
-        unless all_connections.include?(v.name)
-          pendings << v.name
-        end
-      end
-      unless pendings.empty?
-        edges << { from: "_U_", to: "#{d.id}", 
-                   name: pendings.join(",") }
+      in_pendings = _get_pending_connections(d.input_variables)
+      unless in_pendings.empty?
+        edges << { from: "_U_", to: "#{d.id}", name: in_pendings.join(",") }
       end
 
-      pendings = [] 
-      d.output_variables.each do |v|
-        unless all_connections.include?(v.name)
-          pendings << v.name
-        end
-      end 
-      unless pendings.empty?
-        edges << { from: "#{d.id}", to: "_U_", 
-                   name: pendings.join(",") }
+      out_pendings = _get_pending_connections(d.output_variables)
+      unless out_pendings.empty?
+        edges << { from: "#{d.id}", to: "_U_", name: out_pendings.join(",") }
       end        
     end    
     edges
   end
-  
+
   private
+  
+    def _get_pending_connections(vars)
+      pendings = []
+      vars.each do |v|
+        unless @all_connections.include?(v.name)
+          pendings << v.name
+        end
+      end
+      pendings
+    end      
 
     def _create_from_attachment
       attachment.save
