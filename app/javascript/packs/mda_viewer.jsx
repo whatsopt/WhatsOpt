5a5d3c718b023ee6b663e28726df693ed25ad98d
Rémi Lafage <remi.lafage@onera.fr>
2017-05-27 17:47:42 +0200

    first react component for mda view

--- app/javascript/packs/mda_viewer.jsx
+++ app/javascript/packs/mda_viewer.jsx
@@ -1,26 +1,66 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import Graph from 'graph'
+import Xdsm from 'xdsm'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
+class XdsmViewer extends React.Component {
+  componentDidMount() {    
+    /* D3 code to append elements to this.svg */
+  }
+  
+  shouldComponentUpdate() {
+    return false; // This prevents future re-renders of this component
+  }
+  
+  render() {
+    return (
+      <div className="xdsm"></div>
+     );
+  }
 }
 
-Hello.propTypes = {
-  name: PropTypes.string
+class Discipline extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {name: this.props.name}
+  }
+  
+  render() {
+    return <h2>Discipline {this.state.name}</h2>
+  }  
+} 
+
+class Mda extends React.Component {
+  constructor(props) {
+    super(props) 
+    this.state = {name: this.props.name, mda: this.props.mda}
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>MDA {this.state.name}</h1>
+        <XdsmViewer />
+        <Discipline name='Geometry'/>
+      </div>
+    );
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
+    <Mda name="CICAV" mda={MDA}/>,
+    document.getElementById('mda-viewer'),
   )
+  
+  var tooltip = d3.select("body").selectAll(".tooltip").data(['tooltip'])
+      .enter().append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+  
+  console.log(JSON.stringify(MDA));
+  var graph = new Graph(MDA);
+  var xdsm = new Xdsm(graph, 'root', tooltip);
+  xdsm.draw();
 })
