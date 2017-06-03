7abe49f5bddf7958091bf74104ebcf4f4244bf39
Rémi Lafage <remi.lafage@onera.fr>
2017-06-03 21:48:32 +0200

    show connections

--- app/javascript/packs/mda_viewer.jsx
+++ app/javascript/packs/mda_viewer.jsx
@@ -28,9 +28,7 @@ class XdsmViewer extends React.Component {
   }
   
   render() {
-    return (
-      <div className="xdsm"></div>
-     );
+    return ( <div className="xdsm"></div> );
   }
 }
 
@@ -45,24 +43,89 @@ class Discipline extends React.Component {
   }  
 } 
 
+class Connection extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = this.props.conn
+  }
+  
+  render() {
+    return (
+      <tr>
+        <td>{this.state.from}</td> 
+        <td>{this.state.varname}</td> 
+        <td>{this.state.to}</td>
+      </tr>
+    );
+  }    
+}
+
+class Connections extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = this.props.mda;
+  }
+  
+  render() {
+    var conns = [] 
+    this.state.edges.forEach((edge) => {
+      var vars = edge.name.split(",");
+      vars.forEach((v) => {
+        var nameFrom = this._findNode(edge.from).name
+        var nameTo = this._findNode(edge.to).name
+        conns.push({
+          id: nameFrom+'_'+v+nameTo,  
+          from: nameFrom,
+          to: nameTo,
+          varname: v 
+        });
+      }, this);
+    }, this); 
+    
+    var connections = conns.map((conn) => {
+      return ( <Connection key={conn.id} conn={conn}/> );
+    }); 
+    
+    return (
+      <table className="table table-striped connections">
+        <thead>
+          <tr>
+            <th>From</th>
+            <th>Variable</th>
+            <th>To</th>
+          </tr>
+        </thead>
+      
+        <tbody>
+          {connections}
+        </tbody>
+      </table>
+    );
+  }    
+    
+  _findNode(id) {
+    if (id === '_U_') return {id:'_U_', name:'_U_'}
+    for (var i=0; i < this.state.nodes.length; i++) {
+      if (this.state.nodes[i].id === id) {
+        return this.state.nodes[i];
+      } 
+    };
+    throw "Node id ("+ id +") unknown: " + JSON.stringify(this.state.nodes);
+  }
+}
+
 class Mda extends React.Component {
   constructor(props) {
     super(props) 
-    this.state = this.props.mda
+    this.state = this.props.mda;
   }
-   
-  render() {
-    var disciplines = this.state.nodes.map((node) => {
-      return (
-          <Discipline key={node.id} name={node.name} />
-        );
-    }); 
-    
+  
+  render() {    
     return (
       <div>
         <h1>MDA {this.state.name}</h1>
         <XdsmViewer mda={this.state}/>
-        {disciplines}
+        <Connections mda={this.state}/>
       </div>
     );
   }
