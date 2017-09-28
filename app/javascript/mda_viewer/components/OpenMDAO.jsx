734d7c1addb8c5c21683ebd8e7144129dff1fe48
relf <remi.lafage@onera.fr>
2017-09-28 17:30:53 +0200

    add axios config

--- app/javascript/mda_viewer/components/OpenMDAO.jsx
+++ app/javascript/mda_viewer/components/OpenMDAO.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import axios from 'axios';
 
+let token = document.getElementsByName('csrf-token')[0].getAttribute('content')
+axios.defaults.headers.common['X-CSRF-Token'] = token
+axios.defaults.headers.common['Accept'] = 'application/json'
+//axios.defaults.baseURL = 'http://endymion:3000'
+
 class OpenMDAOLogLine extends React.Component {  
   
   constructor(props) {
