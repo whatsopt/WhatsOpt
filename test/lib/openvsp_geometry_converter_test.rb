aa86119c98b3313714299f4e6731ef18fa39d65a
Rémi Lafage <remi.lafage@onera.fr>
2018-01-06 00:54:13 +0100

    skip test when openvsp is not installed

--- test/lib/openvsp_geometry_converter_test.rb
+++ test/lib/openvsp_geometry_converter_test.rb
@@ -4,12 +4,17 @@ require 'tmpdir'
 
 class OpenvspGeometryConverterTest < ActiveSupport::TestCase
 
+  def check_openvsp
+    system('vspscript')
+  end
+  
   def setup
     @filename = "launcher.vsp3"
     @vspconv = WhatsOpt::OpenvspGeometryConverter.new(sample_file(@filename))
   end
   
   test "should generate vspscript" do
+    skip unless check_openvsp
     Dir.mktmpdir do |dir|
       filepath = @vspconv.generate_vspscript dir
       assert File.exists?(filepath)
@@ -17,11 +22,13 @@ class OpenvspGeometryConverterTest < ActiveSupport::TestCase
   end
     
   test "should make an x3d file from a vsp3 file" do
+    skip unless check_openvsp
     dst = @vspconv.convert
     assert File.exist?(dst.path)
   end
   
   test "should generate apologize html when bad file format" do
+    skip unless check_openvsp
     vspconv = WhatsOpt::OpenvspGeometryConverter.new(sample_file("fake_openvsp.vsp3"))
     dst = vspconv.convert
     content = File.new(dst).read
