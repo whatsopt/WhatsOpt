4e75891742658e5dd3942692ddb05fdd89269510
relf <remi.lafage@onera.fr>
2018-01-22 18:07:05 +0100

    add another mime type for uploading notebooks!

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,5 @@
 Paperclip.options[:content_type_mappings] = {
-  ipynb: %w(text/plain text/x-java text/html),
+  ipynb: %w(text/plain text/x-java text/html text/x-c),
   cmdows: %w(application/xml),
   vsp3: %w(application/xml)
 }
