4195616c225e955a34082f0beb15aa876c617655
relf <remi.lafage@onera.fr>
2018-01-23 10:27:03 +0100

    fix notebook tests

--- test/controllers/notebook_controller_test.rb
+++ test/controllers/notebook_controller_test.rb
@@ -9,13 +9,13 @@ class NotebookControllerTest < ActionDispatch::IntegrationTest
 
   test "should assign owner on creation" do
     assert_difference('Notebook.count') do
-      post notebooks_url, params: { notebook: {attachment_attributes: { data: @nb }} }
+      post notebooks_url, params: { notebook: {title: "Test", attachment_attributes: { data: @nb }} }
     end
     assert Notebook.last.owner, users(:user1)  
   end
   
   test "should not destroy notebook, if not owner" do
-    post notebooks_url, params: { notebook: {attachment_attributes: { data: @nb }} }
+    post notebooks_url, params: { notebook: {title: "Test", attachment_attributes: { data: @nb }} }
     sign_out users(:user1)
     sign_in users(:user2)
     assert_difference('Notebook.count', 0) do
