c2c23564d6938659ff5afe3c2178340b472b2d0b
relf <remi.lafage@onera.fr>
2018-01-31 11:15:04 +0100

    fix setup for tabulate

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='whatsopt',
-      version='0.1.0',
+      version='0.1.1',
       description='Remote client command line tool',
       url='http://gitlab.com/relf/WhatsOpt',
       author='Remi Lafage',
@@ -9,7 +9,7 @@ setup(name='whatsopt',
       license='Apache 2.0',
       packages=['whatsopt'],
       install_requires=[
-          'Click', 'beautifultable'
+          'Click', 'tabulate'
       ],
       entry_points="""
           [console_scripts]
