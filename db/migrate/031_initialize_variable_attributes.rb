e05cd1b07a84ce1c5386c148386197504b36fda3
relf <remi.lafage@onera.fr>
2018-02-20 15:43:19 +0100

    initialize only if blank

--- db/migrate/031_initialize_variable_attributes.rb
+++ db/migrate/031_initialize_variable_attributes.rb
@@ -2,10 +2,11 @@ class InitializeVariableAttributes < ActiveRecord::Migration[5.1]
   def up
     Variable.all.each do |v|
       p v
-      desc = "" if v.desc.blank?
-      units = "" if v.units.blank?
-      fullname = v.name if v.fullname.blank?
-      v.update_columns(desc: desc, units: units, fullname: fullname)
+      columns={}
+      columns[:desc] = "" if v.desc.blank?
+      columns[:units] = "" if v.units.blank?
+      columns[:fullname] = v.name if v.fullname.blank?
+      v.update_columns(columns)
     end
 
     Parameter.all.each do |param|
