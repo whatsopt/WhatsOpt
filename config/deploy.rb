53684754f984a16f19335f497b9d7ab7fb93049c
relf <remi.lafage@onera.fr>
2018-02-20 15:26:15 +0100

    avoid validation for initialization

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,3 +24,15 @@ SSHKit.config.command_map[:rake]  = "bundle exec rake"
 SSHKit.config.command_map[:rails] = "bundle exec rails"
 
 
+before "deploy:assets:precompile", "deploy:yarn_install"
+
+namespace :deploy do
+  desc 'Run rake yarn:install'
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install")
+      end
+    end
+  end
+end
--- db/migrate/031_initialize_variable_attributes.rb
+++ db/migrate/031_initialize_variable_attributes.rb
@@ -2,10 +2,10 @@ class InitializeVariableAttributes < ActiveRecord::Migration[5.1]
   def up
     Variable.all.each do |v|
       p v
-      v.desc = "" if v.desc.blank?
-      v.units = "" if v.units.blank?
-      v.fullname = v.name if v.fullname.blank?
-      v.save!
+      desc = "" if v.desc.blank?
+      units = "" if v.units.blank?
+      fullname = v.name if v.fullname.blank?
+      v.update_columns(desc: desc, units: units, fullname: fullname)
     end
 
     Parameter.all.each do |param|
