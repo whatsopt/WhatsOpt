42bac8424f393a503e663b0b9b4c4b3fd6368193
relf <remi.lafage@onera.fr>
2018-03-05 12:03:20 +0100

    adjust selene deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 set :stage, :production
 
-set :deploy_to, "/var/www/html/rails/whatsopt"
+set :deploy_to, "/dtis-app/whatsopt"
 
-set :rvm_ruby_version, "ruby-2.3.3@whatsopt-staging"
+set :rvm_ruby_version, "ruby-2.3.3@whatsopt-production"
 set :server_name, "selene"
 server 'selene', user: 'rlafage', roles: %w{app web db}, primary: true
 
