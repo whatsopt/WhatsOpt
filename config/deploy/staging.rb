52073054d5e0d912218814b5afca01bcdb6766a7
relf <remi.lafage@onera.fr>
2018-05-03 16:58:00 +0200

    staging deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,5 @@
 set :stage, :staging
 
-set :repo_url, 'ssh://designlab@endymion/iesta-base/designlab/gitrepos/WhatsOpt.git'
-#set :branch, :plotter
-
 set :deploy_to, "/var/www/html/rails/whatsopt"
 
 set :rvm_ruby_version, "ruby-2.3.3@whatsopt-staging"
