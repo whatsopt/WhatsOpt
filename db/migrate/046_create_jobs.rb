edb41cacf80f4b5f5da2e7c3cb649e0e9c01057b
relf <remi.lafage@onera.fr>
2018-07-02 17:25:10 +0200

    fix migration as it seems that MySQL do not allow to default value for a text column

--- db/migrate/046_create_jobs.rb
+++ db/migrate/046_create_jobs.rb
@@ -2,7 +2,7 @@ class CreateJobs < ActiveRecord::Migration[5.2]
   def change
     create_table :jobs do |t|
       t.string :status
-      t.text :log, default: ""
+      t.text :log
       t.integer :pid, default: -1
       t.integer :operation_id
       t.datetime :started_at
