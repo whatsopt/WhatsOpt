0aa4b61826112d783de7568cc9f7ba89f33b801d
relf <remi.lafage@onera.fr>
2018-09-28 11:17:56 +0200

    add jobs controller

---
+++ app/controllers/api/v1/jobs_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::JobsController < Api::ApiController
+  before_action :set_job, only: [:create, :update]
+    
+  def create
+    OperationJob.perform_later(current_user, @operation)
+  end
+  
+  def update
+    if @job.pid > -1
+      begin
+        Process.kill("KILL", @job.pid)
+      rescue Exception => e
+        Rails.logger.info e
+      end
+      @job.update(pid: -1, status: "FAILED", log: @job.log << 'Process Aborted\n')
+    end
+  end
+    
+  private
+    def set_job
+      @operation = Operation.find(params[:operation_id])
+      @job = @operation.job || ope.create_job(status: 'PENDING', pid: -1, log: "")
+      authorize @operation.analysis
+    end
+  
+    def job_params
+      params.require(:job).permit(:pid, :status, :log)
+    end
+    
+end
