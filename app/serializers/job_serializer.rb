6116bdc0a10a97c9c63cb911f9c55f8746bdb4be
relf <remi.lafage@onera.fr>
2018-11-13 14:46:18 +0100

    add operation job timing

---
+++ app/serializers/job_serializer.rb
@@ -0,0 +1,10 @@
+class JobSerializer < ActiveModel::Serializer
+  attributes :status, :log, :start_in_ms, :end_in_ms
+  
+  def start_in_ms
+    object.started_at.to_f * 1000
+  end
+  def end_in_ms
+    object.ended_at.to_f * 1000
+  end
+end
