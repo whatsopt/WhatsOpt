77c7d960733facdda6e1ac28b69e85b75f475e6b
relf <remi.lafage@onera.fr>
2018-11-15 11:07:03 +0100

    fix user controller bug

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,7 +14,19 @@ class ActiveSupport::TestCase
 end
 
 class ActionDispatch::IntegrationTest
-    include Devise::Test::IntegrationHelpers
+  include Devise::Test::IntegrationHelpers
+  
+#  # Make the Capybara DSL available in all integration tests
+#  include Capybara::DSL
+#  # Make `assert_*` methods behave like Minitest assertions
+#  include Capybara::Minitest::Assertions
+#
+#  # Reset sessions and driver between tests
+#  # Use super wherever this method is redefined in your individual test classes
+#  def teardown
+#    Capybara.reset_sessions!
+#    Capybara.use_default_driver
+#  end
 end
 
 TEST_API_KEY = 'FriendlyApiKey'
