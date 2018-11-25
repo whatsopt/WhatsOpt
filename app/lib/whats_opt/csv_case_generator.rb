f19886d5a4392af954d15f0eeb5a4eec3bd7b755
Rémi Lafage <remi.lafage@onera.fr>
2018-11-25 15:38:26 +0100

    refactor csv_generator.rb

--- app/lib/whats_opt/csv_case_generator.rb
+++ app/lib/whats_opt/csv_case_generator.rb
@@ -2,17 +2,11 @@ require 'zip'
 require 'csv'
 
 module WhatsOpt  
-  class CsvCaseGenerator
-
-    def initialize(zip: false)
-      @zip = !!zip
-    end
+  class CsvCaseGenerator < CsvGenerator
             
-    def generate(cases)
-      basename = "cases"
+    def _generate(cases)
+      @basename = "cases"
 
-      return "", "#{basename}.zip" if cases.empty?
-            
       headers = cases.map do |c| 
         varname = c.variable&.name || "unknown_#{c.variable_id}"
         if c.coord_index > -1 
@@ -26,23 +20,7 @@ module WhatsOpt
       cases0 = vals.shift 
       values = cases0.zip(*vals)
     
-      content = CSV.generate(col_sep: ';') do |csv|
-        csv << headers
-        values.each do |v|
-          csv << v
-        end
-      end
-            
-      if @zip
-        stringio = Zip::OutputStream::write_buffer do |zio|
-          zio.put_next_entry("#{basename}.csv") 
-          zio.write content
-        end
-        stringio.rewind
-        return stringio.read, "#{basename}.zip"
-      else
-        return content, "#{basename}.csv"
-      end
+      return headers, values
     end
     
   end
