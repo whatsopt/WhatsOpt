758d2347bcc0ff6f75698af4227f9f09eca4d871
relf <remi.lafage@onera.fr>
2018-12-06 12:34:20 +0100

    fix foreign_key type for table created with 5.0

--- db/migrate/053_create_analysis_disciplines.rb
+++ db/migrate/053_create_analysis_disciplines.rb
@@ -1,8 +1,8 @@
 class CreateAnalysisDisciplines < ActiveRecord::Migration[5.2]
   def change
     create_table :analysis_disciplines do |t|
-      t.references :discipline, foreign_key: true
-      t.references :analysis, foreign_key: true
+      t.references :discipline, foreign_key: true, type: :int
+      t.references :analysis, foreign_key: true, type: :int
     end
   end
 end
