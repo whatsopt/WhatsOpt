0bbdcb2c0b21a54388c408763e6739ef50a98fe9
relf <remi.lafage@onera.fr>
2019-04-11 16:30:46 +0200

    remove foreign_key contraint : pb with mysql config

--- db/migrate/060_create_scalings.rb
+++ db/migrate/060_create_scalings.rb
@@ -1,7 +1,7 @@
 class CreateScalings < ActiveRecord::Migration[5.2]
   def change
     create_table :scalings do |t|
-      t.references :variable, foreign_key: true
+      t.references :variable, index: true
       t.string :ref, default: ""
       t.string :ref0, default: ""
       t.string :res_ref, default: ""
