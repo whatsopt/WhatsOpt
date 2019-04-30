03ad4bba60b9acba7bbb2af944e7a3d49a65d90a
Rémi Lafage <remi.lafage@onera.fr>
2019-04-30 18:47:05 +0200

    test thrift presence for export test

--- test/controllers/exports_controller_test.rb
+++ test/controllers/exports_controller_test.rb
@@ -1,12 +1,20 @@
 require 'test_helper'
+require 'mkmf' # for find_executable
+MakeMakefile::Logging.instance_variable_set(:@log, File.open(File::NULL, 'w'))
 
 class ExportsControllerTest < ActionDispatch::IntegrationTest
+  
+  def thrift?
+    found ||= find_executable("thrift")
+  end
+
   setup do
     sign_in users(:user1)
     @mda = analyses(:cicav)
   end
   
   test "should get new openmdao zip archive given an mda_id" do
+    skip "Apache Thrift not installed" unless thrift?
     get mda_exports_new_url(mda_id: @mda.id, format: :openmdao)
     assert_response :success
   end
