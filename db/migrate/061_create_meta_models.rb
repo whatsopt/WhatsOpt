cb750f0d6ff8abcd26c34de0500c92f4ef85c53b
relf <remi.lafage@onera.fr>
2019-08-27 14:26:44 +0200

    fix deployment: repo url, mysql migration

--- db/migrate/061_create_meta_models.rb
+++ db/migrate/061_create_meta_models.rb
@@ -1,8 +1,8 @@
 class CreateMetaModels < ActiveRecord::Migration[5.2]
   def change
     create_table :meta_models do |t|
-      t.references :discipline, foreign_key: true
-      t.references :operation, foreign_key: true
+      t.references :discipline
+      t.references :operation
 
       t.string :default_surrogate_kind
 
--- db/migrate/062_create_surrogates.rb
+++ db/migrate/062_create_surrogates.rb
@@ -1,8 +1,8 @@
 class CreateSurrogates < ActiveRecord::Migration[5.2]
   def change
     create_table :surrogates do |t|
-      t.references :meta_model, foreign_key: true
-      t.references :variable, foreign_key: true
+      t.references :meta_model
+      t.references :variable
       t.integer :coord_index 
       t.string :kind
       t.string :status
