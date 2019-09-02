f6e64490a033d253c9e9125fab79c0f910a1468b
relf <remi.lafage@onera.fr>
2019-09-02 14:58:59 +0200

    disable notebooks access

--- app/policies/notebook_policy.rb
+++ app/policies/notebook_policy.rb
@@ -3,22 +3,22 @@
 class NotebookPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      if intranet?
-        scope.all
-      else
+      # if intranet?
+      #   scope.all
+      # else
         scope.none
-      end
+      # end
     end
   end
-  def create?
-    intranet?
-  end
+  # def create?
+  #   # intranet?
+  # end
 
-  def update?
-    intranet? && (@user.admin? || @user.has_role?(:owner, @record))
-  end
+  # def update?
+  #   # intranet? && (@user.admin? || @user.has_role?(:owner, @record))
+  # end
 
-  def destroy?
-    intranet? && (@user.admin? || @user.has_role?(:owner, @record))
-  end
+  # def destroy?
+  #   # intranet? && (@user.admin? || @user.has_role?(:owner, @record))
+  # end
 end
