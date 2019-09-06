32204592a4c6860ff587341c50476ea264b99bf1
relf <remi.lafage@onera.fr>
2019-09-06 10:15:31 +0200

    fix relative url root in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # set the relative root, because we're deploying to /whatsopt
+  config.relative_url_root  = "/whatsopt"
+
 end
