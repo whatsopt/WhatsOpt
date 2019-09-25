d172fddab32e9f37086dd77016d2dba40c8ff54a
relf <remi.lafage@onera.fr>
2019-09-25 10:19:06 +0200

    authorize metamodel prediction for anybody

--- test/controllers/api/v1/meta_models_controller_test.rb
+++ test/controllers/api/v1/meta_models_controller_test.rb
@@ -4,10 +4,11 @@ require "test_helper"
 
 class Api::V1::MetaModelsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @auth_headers = { "Authorization" => "Token " + TEST_API_KEY }
     @mda = analyses(:cicav)
     @ope = operations(:doe)
     @user = users(:user1)
+    @auth_headers = { "Authorization" => "Token " + @user.api_key }
+    @user2 = users(:user2)
   end
 
   teardown do
@@ -24,6 +25,15 @@ class Api::V1::MetaModelsControllerTest < ActionDispatch::IntegrationTest
     responses = resp["responses"]
     assert_in_delta(2.32, responses[0][0])
     assert_in_delta(7.841, responses[1][0])
+  end
+
+  test "anybody can make prediction" do
+    mm = meta_models(:cicav_metamodel)
+    @auth_headers = { "Authorization" => "Token " + @user2.api_key }
+    put api_v1_meta_model_url(mm), params: {meta_model: {
+        format: 'matrix', values: [[3, 5, 7], [6, 10, 1]]
+      }}, as: :json, headers: @auth_headers
+    assert_response :success
   end
 
 end
