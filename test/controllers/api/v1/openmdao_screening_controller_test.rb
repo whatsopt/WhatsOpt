51895eae514828755d19a34467e8fb4581a1add6
Rémi Lafage <remi.lafage@onera.fr>
2019-12-27 21:11:30 +0100

    fix test

--- test/controllers/api/v1/openmdao_screening_controller_test.rb
+++ test/controllers/api/v1/openmdao_screening_controller_test.rb
@@ -13,10 +13,11 @@ class Api::V1::OpenmdaoScreeningControllerTest < ActionDispatch::IntegrationTest
     get api_v1_operation_openmdao_screening_url(@ope), as: :json, headers: @auth_headers
     assert_response :success
     sa = JSON.parse(response.body)
-    expected = { "obj" => { "mu" => [0.65, 0.21000000000000005],
-                          "mu_star" => [1.62, 0.3500000000000001],
-                          "sigma" => [1.794107020219251, 0.41199514560246936],
-                          "names" => ["x1", "z"] } }
+    expected = {"saMethod"=>"morris", "saResult"=>{
+                  "obj"=>{"mu"=>[0.65, 0.21000000000000005], 
+                  "mu_star"=>[1.62, 0.3500000000000001], 
+                  "sigma"=>[1.794107020219251, 0.41199514560246936], 
+                  "parameter_names"=>["x1", "z"]}}}
     assert_equal expected, sa["sensitivity"]
   end
 end
