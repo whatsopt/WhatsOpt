fbf2281d03e307ec944fc85bacb08ca588e731d9
relf <remi.lafage@onera.fr>
2020-01-14 18:09:15 +0100

    init AR serializer for nested structures

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -1,2 +1,5 @@
 require 'active_model_serializers'
 ActiveModelSerializers.logger = Logger.new(nil)
+
+# WARNING: should avoid recursive def => avoid defining has_* AND belongs_to in serializers. We define only has_*
+ActiveModel::Serializer.config.default_includes = '**' # (default '*')
