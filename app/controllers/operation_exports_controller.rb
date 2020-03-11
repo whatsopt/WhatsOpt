aff49bcce86b72b8e6c1389ef839b41fc7fd5fc2
relf <remi.lafage@onera.fr>
2020-03-11 18:11:52 +0100

    allow exporting csv data files as soon as you have access to the analysis

--- app/controllers/operation_exports_controller.rb
+++ app/controllers/operation_exports_controller.rb
@@ -5,7 +5,7 @@ require "whats_opt/csv_case_generator"
 class OperationExportsController < ApplicationController
   def new
     @ope = Operation.find(params[:operation_id])
-    authorize @ope
+    authorize @ope.analysis
     csvgen = WhatsOpt::CsvCaseGenerator.new
     content, filename = csvgen.generate @ope.sorted_cases, @ope.success
     send_data content, filename: filename
