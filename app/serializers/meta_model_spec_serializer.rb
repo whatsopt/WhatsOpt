5980dfda46b1e5fca50efd8224567eca5049712a
relf <remi.lafage@onera.fr>
2020-06-17 11:55:35 +0200

    mm api with ref analysis

--- app/serializers/meta_model_spec_serializer.rb
+++ app/serializers/meta_model_spec_serializer.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class MetaModelSpecSerializer < ActiveModel::Serializer
-  attributes :id, :reference_analysis, :xlabels, :ylabels
+  attributes :id, :surrogate_kind, :reference_analysis, :xlabels, :ylabels
+
+  def surrogate_kind
+    object.default_surrogate_kind
+  end
 
   def reference_analysis
     AnalysisSerializer.new(object.analysis).as_json
