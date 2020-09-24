0a3b7f992e2aa3d799758ac70ea77cae50864211
relf <remi.lafage@onera.fr>
2020-09-24 15:19:41 +0200

    implement user deactivation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_27_210649) do
+ActiveRecord::Schema.define(version: 2020_09_24_122931) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -285,6 +285,7 @@ ActiveRecord::Schema.define(version: 2020_06_27_210649) do
     t.datetime "updated_at", null: false
     t.string "api_key"
     t.text "settings"
+    t.boolean "deactivated"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
