6ee644841726a87b518a1a80883ff8b8f173703a
relf <remi.lafage@onera.fr>
2021-05-05 17:46:26 +0200

    Fix analyses list in design project page

--- app/helpers/design_projects_helper.rb
+++ app/helpers/design_projects_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module DesignProjectsHelper
-  def link_to_analyses_if_authorized(design_project)
+  def link_to_analyses_if_authorized(design_project, nb=3)
     res = ""
-    design_project.analyses.each do |mda|
+    design_project.analyses.roots.each do |mda|
       res += '<span style="margin: 0px 5px">'
       if policy(mda).show?
         res += link_to mda.name, mda_path(mda)
