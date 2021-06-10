49fe91eb59ee1bae2aa057af14c664a6cd18876f
relf <remi.lafage@onera.fr>
2021-06-10 15:38:03 +0200

    Fix pagination disabled state

--- app/javascript/mda_viewer/components/VariablesPagination.jsx
+++ app/javascript/mda_viewer/components/VariablesPagination.jsx
@@ -20,52 +20,58 @@ class VariablesPagination extends React.Component {
             pageSize
         } = this.props;
 
+        const pageItemPrev = `page-item ${canPreviousPage ? "" : "disabled"}`;
+        const pageItemNext = `page-item ${canNextPage ? "" : "disabled"}`;
+
         return (
-            <div className="pagination">
-                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-                    {'<<'}
-                </button>{' '}
-                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-                    {'<'}
-                </button>{' '}
-                <button onClick={() => nextPage()} disabled={!canNextPage}>
-                    {'>'}
-                </button>{' '}
-                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-                    {'>>'}
-                </button>{' '}
-                <span>
-                    Page{' '}
-                    <strong>
-                        {pageIndex + 1} of {pageOptions.length}
-                    </strong>{' '}
-                </span>
-                <span>
-                    | Go to page:{' '}
-                    <input
-                        type="number"
-                        defaultValue={pageIndex + 1}
-                        onChange={e => {
-                            const page = e.target.value ? Number(e.target.value) - 1 : 0
-                            gotoPage(page)
-                        }}
-                        style={{ width: '100px' }}
-                    />
-                </span>{' '}
-                <select
-                    value={pageSize}
-                    onChange={e => {
-                        setPageSize(Number(e.target.value))
-                    }}
-                >
-                    {[10, 20, 30, 40, 50].map(pageSize => (
-                        <option key={pageSize} value={pageSize}>
-                            Show {pageSize}
-                        </option>
-                    ))}
-                </select>
-            </div>
-        )
+            <div className="editor-section">
+                <nav>
+                    <ul className="pagination" style={{ marginBottom: 0 }}>
+                        <li className={pageItemPrev}>
+                            <button className="page-link" onClick={() => gotoPage(0)} aria-label="Previous">
+                                <i className="fas fa-angle-double-left"></i>
+                            </button>
+                        </li>
+                        <li className={pageItemPrev}>
+                            <button className="page-link" onClick={() => previousPage()} aria-label="Previous">
+                                <i className="fas fa-angle-left"></i>
+                            </button>
+                        </li>
+                        <li className="page-item disabled"><span className="page-link">{pageIndex + 1} / {pageOptions.length}</span></li>
+
+                        <li className={pageItemNext}>
+                            <button className="page-link" onClick={() => nextPage()} aria-label="Previous">
+                                <i className="fas fa-angle-right"></i>
+                            </button>
+                        </li>
+                        <li className={pageItemNext}>
+                            <button className="page-link" onClick={() => gotoPage(pageCount - 1)} aria-label="Previous">
+                                <i className="fas fa-angle-double-right"></i>
+                            </button>
+                        </li>
+                        <span className="ml-2" />
+                        <li className="page-item">
+                            <div className="form-row align-items-center">
+                                <div className="form-group col-auto">
+                                    <select className="form-control"
+                                        value={pageSize}
+                                        onChange={e => {
+                                            setPageSize(Number(e.target.value))
+                                        }}
+                                    >
+                                        {[10, 20, 30, 50, 100].map(pageSize => (
+                                            <option key={pageSize} value={pageSize}>
+                                                Show {pageSize}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+            </div >
+        );
     }
 }
 
