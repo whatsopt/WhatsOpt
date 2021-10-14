e82aeede04401a91c4288f685bddb0c195c369ef
relf <remi.lafage@onera.fr>
2021-10-14 17:35:30 +0200

    Improve diff visualization

--- app/lib/whats_opt/analysis_diff.rb
+++ app/lib/whats_opt/analysis_diff.rb
@@ -5,11 +5,11 @@ require 'tempfile'
 class WhatsOpt::AnalysisDiff
 
     def self.compare(mda1, mda2)
-        mda1_file = Tempfile.new("#{mda1.basename}_#{mda1.id}")
+        mda1_file = Tempfile.new("#{mda1.basename}_#{mda1.id}__")
         str1 = JSON.pretty_generate(AnalysisDiffSerializer.new(mda1).as_json)
         mda1_file.write(str1)
         mda1_file.flush
-        mda2_file = Tempfile.new("#{mda2.basename}_#{mda2.id}")
+        mda2_file = Tempfile.new("#{mda2.basename}_#{mda2.id}__")
         str2 = JSON.pretty_generate(AnalysisDiffSerializer.new(mda2).as_json)
         mda2_file.write(str2)
         mda2_file.flush
