3637d24da40767bdcd7d5f08934310e23ebfba3d
relf <remi.lafage@onera.fr>
2022-09-02 15:07:46 +0200

    Try fix from issue react-table #3297

--- app/javascript/mda_viewer/components/VariablesGlobalFilter.jsx
+++ app/javascript/mda_viewer/components/VariablesGlobalFilter.jsx
@@ -2,7 +2,50 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import { useAsyncDebounce } from 'react-table';
+// import { useAsyncDebounce } from 'react-table';
+
+function useGetLatest(obj) {
+  const ref = React.useRef();
+  ref.current = obj;
+
+  return React.useCallback(() => ref.current, []);
+}
+
+export function useAsyncDebounce(defaultFn, defaultWait = 0) {
+  const debounceRef = React.useRef({});
+
+  const getDefaultFn = useGetLatest(defaultFn);
+  const getDefaultWait = useGetLatest(defaultWait);
+
+  return React.useCallback(
+    async (...args) => {
+      if (!debounceRef.current.promise) {
+        debounceRef.current.promise = new Promise((resolve, reject) => {
+          debounceRef.current.resolve = resolve;
+          debounceRef.current.reject = reject;
+        });
+      }
+
+      if (debounceRef.current.timeout) {
+        clearTimeout(debounceRef.current.timeout);
+      }
+
+      debounceRef.current.timeout = setTimeout(async () => {
+        delete debounceRef.current.timeout;
+        try {
+          debounceRef.current.resolve(await getDefaultFn()(...args));
+        } catch (err) {
+          debounceRef.current.reject(err);
+        } finally {
+          delete debounceRef.current.promise;
+        }
+      }, getDefaultWait());
+
+      return debounceRef.current.promise;
+    },
+    [getDefaultFn, getDefaultWait],
+  );
+}
 
 function VariablesGlobalFilter({
   globalFilteredRows,
