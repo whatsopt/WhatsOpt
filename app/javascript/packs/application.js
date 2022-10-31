567875923044a0333b670ae0802d95e21b9a71ff
rlafage <remi.lafage@onera.fr>
2022-10-31 16:03:58 +0100

    Override default confirm dialog with a bootstrap modal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,6 @@ import OperationsIndex from '../application_views/operations_index';
 import OptimizationsIndex from '../application_views/optimizations_index';
 import UserShow from '../application_views/users_show';
 
-Rails.start();
-
 // Views are used in corresponding views/**/*.html.erb files
 // in document_ready section.
 // Calling start() allow to attach js callbacks to DOM element
@@ -16,3 +14,24 @@ window.App = {
   OptimizationsIndex,
   UserShow,
 };
+
+// Override default browser confirm dialog by bootstrap version
+Rails.confirm = (message, element) => {
+  const $element = $(element);
+  const $dialog = $('#confirmModal');
+  const $content = $dialog.find('#modal-content');
+  const $ok = $dialog.find('#ok-button');
+  $content.text(message);
+  $ok.on('click', (event) => {
+    event.preventDefault();
+    $dialog.modal('hide');
+    const old = Rails.confirm;
+    Rails.confirm = () => true;
+    $element.get(0).click();
+    Rails.confirm = old;
+  });
+  $dialog.modal('show');
+  return false;
+};
+
+Rails.start();
