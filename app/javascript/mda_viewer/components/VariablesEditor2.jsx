31bd32a2de9d4f6317350069ba93965397141d1d
rlafage <remi.lafage@onera.fr>
2022-11-07 12:19:33 +0100

    Customize cells

--- app/javascript/mda_viewer/components/VariablesEditor2.jsx
+++ app/javascript/mda_viewer/components/VariablesEditor2.jsx
@@ -14,6 +14,283 @@ import { RIEInput, RIESelect } from './riek/src';
 import VariablesPagination from './VariablesPagination';
 import VariablesGlobalFilter from './VariablesGlobalFilter';
 
+const CELL_CLASSNAME = 'react-table-cell';
+// const EDITABLE_CELL_CLASSNAME = 'bg-light';
+const EDITABLE_CELL_CLASSNAME = 'editable-react-table-cell';
+
+function _computeRoleSelection(conn) {
+  const options = [
+    { id: 'parameter', text: 'Parameter' },
+    { id: 'design_var', text: 'Design Variable' },
+    { id: 'uncertain_var', text: 'Uncertain Variable' },
+
+    { id: 'response', text: 'Response' },
+    { id: 'response_of_interest', text: 'Response of interest' },
+    { id: 'min_objective', text: 'Min Objective' },
+    { id: 'max_objective', text: 'Max Objective' },
+    { id: 'ineq_constraint', text: 'Neg Constraint' },
+    { id: 'pos_constraint', text: 'Pos Constraint' },
+    { id: 'eq_constraint', text: 'Eq Constraint' },
+    { id: 'constraint', text: 'Constraint' },
+    { id: 'state_var', text: 'State Variable' }];
+  if (conn.role === 'parameter' || conn.role === 'design_var' || conn.role === 'uncertain_var') {
+    options.splice(3); // remove outputs and state vars
+  } else {
+    // options.splice(options.length - 1, 1); // remove state_var
+    options.splice(0, 3); // remove inputs
+  }
+  return options;
+}
+
+// eslint-disable-next-line no-unused-vars
+function _computeTypeSelection(conn) {
+  const options = [
+    { id: 'Float', text: 'Float' },
+    { id: 'Integer', text: 'Integer' },
+    { id: 'String', text: 'String' },
+  ];
+  return options;
+}
+
+function CheckButtonCell({
+  cell,
+  row: { index },
+  table: { options: { data: connections, meta: { limited } } },
+}) {
+  const isChecked = connections[index].active;
+  return (
+    <input
+      type="checkbox"
+      value={cell.getValue()}
+      checked={isChecked}
+      onChange={() => onConnectionChange(
+        connections[index].id,
+        { active: !isChecked },
+      )}
+      disabled={limited}
+    />
+  );
+}
+
+CheckButtonCell.propTypes = {
+  cell: PropTypes.shape({
+    value: PropTypes.bool.isRequired,
+  }).isRequired,
+  row: PropTypes.shape({
+    index: PropTypes.number.isRequired,
+  }).isRequired,
+  data: PropTypes.array.isRequired,
+  onConnectionChange: PropTypes.func.isRequired,
+  limited: PropTypes.bool.isRequired,
+};
+
+function ReadonlyCell({
+  cell,
+  row: { index },
+  column: { id },
+  table: { options: { data: connections } },
+}) {
+  let textStyle = CELL_CLASSNAME;
+  textStyle += connections[index].active ? '' : ' text-inactive';
+  let info = cell.getValue();
+  if (id === 'role') {
+    const selectOptions = _computeRoleSelection(connections[index]);
+    for (let i = 0; i < selectOptions.length; i += 1) {
+      if (info === selectOptions[i].id) {
+        info = selectOptions[i].text;
+        break;
+      }
+    }
+  }
+  if (id === 'type') {
+    const selectOptions = _computeTypeSelection(connections[index]);
+    for (let i = 0; i < selectOptions.length; i += 1) {
+      if (info === selectOptions[i].id) {
+        info = selectOptions[i].text;
+        break;
+      }
+    }
+  }
+  if (id === 'name') {
+    const title = connections[index].desc;
+    if (title) {
+      textStyle += ' table-tooltip';
+      return (<span className={textStyle} title={title} data-original-title={title}>{info}</span>);
+    }
+  }
+  return (<span className={textStyle}>{info}</span>);
+}
+
+ReadonlyCell.propTypes = {
+  cell: PropTypes.object.isRequired,
+  row: PropTypes.object.isRequired,
+  column: PropTypes.object.isRequired,
+  data: PropTypes.array.isRequired,
+};
+
+function _uqLabelOf(uq) {
+  const { kind, options_attributes } = uq;
+  const options = options_attributes.map((opt) => opt.value);
+  return `${kind[0]}(${options.join(', ')})`;
+}
+
+function ButtonCell({
+  cell,
+  row,
+  column,
+  table,
+}) {
+  const { options: { data: connections, meta: { isEditing } } } = table;
+  const { index } = row;
+  const {
+    name, role, shape, uq,
+  } = connections[index];
+  let label = uq.length > 0 ? _uqLabelOf(uq[0]) : '';
+  label = uq.length > 1 ? `[${label}, ...]` : label;
+  if (isEditing) {
+    const isEditable = (role === 'uncertain_var');
+    if (isEditable) {
+      return (
+        <button
+          type="button"
+          className={`btn btn-sm ${CELL_CLASSNAME} ${EDITABLE_CELL_CLASSNAME}`}
+          style={{ paddingTop: 0, paddingBottom: 0 }}
+          onClick={() => {
+            if (shape === '1' || shape === '(1,)') {
+              // eslint-disable-next-line no-undef
+              $(`#distributionModal-${name}`).modal('show');
+            } else {
+              // eslint-disable-next-line no-undef
+              $(`#distributionListModal-${name}`).modal('show');
+            }
+          }}
+        >
+          {label || 'No'}
+        </button>
+      );
+    }
+  }
+
+  cell.getValue = () => label;
+  return ReadonlyCell({
+    cell, row, column, table,
+  });
+}
+
+function EditableCell({
+  cell,
+  row,
+  column,
+  table,
+}) {
+  const {
+    options: {
+      data: connections, meta: {
+        onConnectionChange, isEditing, limited, cellToFocus,
+      },
+    },
+  } = table;
+  const value = cell.getValue();
+  const { index } = row;
+  const { id } = column;
+  if (isEditing && !(limited && id === 'type') && connections[index].active) {
+    let selectOptions;
+    if (id === 'role') {
+      selectOptions = _computeRoleSelection(connections[index]);
+    }
+    if (id === 'type') {
+      selectOptions = _computeTypeSelection(connections[index]);
+    }
+
+    const myRef = React.useRef(null);
+
+    if (selectOptions) {
+      const selected = selectOptions.filter((choice) => choice.id === value);
+      return (
+        <RIESelect
+          className={`${CELL_CLASSNAME} ${EDITABLE_CELL_CLASSNAME}`}
+          value={{ id: value, text: selected.length > 0 ? selected[0].text : 'undefined' }}
+          change={(attr) => {
+            const change = {};
+            change[id] = attr[id].id;
+            onConnectionChange(connections[index].id, change);
+          }}
+          propName={id}
+          afterFinish={() => {
+            // eslint-disable-next-line no-param-reassign
+            cellToFocus.current = null;
+            // console.log(cellToFocus.current);
+          }}
+          afterStart={() => {
+            // eslint-disable-next-line no-param-reassign
+            cellToFocus.current = { index, id };
+            // console.log(cellToFocus.current);
+          }}
+          ref={myRef}
+          shouldBlockWhileLoading
+          options={selectOptions}
+        />
+      );
+    }
+
+    React.useEffect(() => {
+      if (cellToFocus.current
+        && cellToFocus.current.index === index
+        && cellToFocus.current.id === id) {
+        if (myRef.current.myRef.current) { // defensive programming
+          myRef.current.myRef.current.click();
+        }
+      }
+    });
+
+    // Editable fields regarding variable role
+    const { role } = connections[index];
+    const isEditable = (((id === 'name' || id === 'shape') && !limited) || id === 'desc' || id === 'units'
+      || id === 'ref' || id === 'ref0' || id === 'res_ref')
+      || (role === 'state_var' && id === 'init')
+      || (role === 'response' && id === 'init')
+      || (role === 'ineq_constraint' && id === 'upper')
+      || (role === 'pos_constraint' && id === 'lower')
+      || (role === 'eq_constraint' && id === 'init')
+      || (role === 'constraint' && (id === 'lower' || id === 'upper'))
+      || (role === 'parameter' && (id === 'init' || id === 'lower' || id === 'upper'))
+      || (role === 'design_var' && (id === 'init' || id === 'lower' || id === 'upper'))
+      || (role === 'uncertain_var' && (id === 'init' || id === 'uq'))
+      || (connections[index].shouldBeBounded && (id === 'lower' || id === 'upper'));
+    if (isEditable) {
+      return (
+        <RIEInput
+          editProps={{ size: 8 }}
+          className={`${CELL_CLASSNAME} ${EDITABLE_CELL_CLASSNAME}`}
+          value={value || ''}
+          change={(attr) => onConnectionChange(connections[index].id, attr)}
+          propName={id}
+          afterFinish={() => {
+            // eslint-disable-next-line no-param-reassign
+            cellToFocus.current = null;
+            // console.log(cellToFocus.current);
+          }}
+          afterStart={() => {
+            // eslint-disable-next-line no-param-reassign
+            cellToFocus.current = { index, id };
+            // console.log(cellToFocus.current);
+          }}
+          ref={myRef}
+          shouldBlockWhileLoading
+        />
+      );
+    }
+  }
+  return ReadonlyCell({
+    cell, row, column, table,
+  });
+}
+
+// Set our editable cell renderer as the default Cell renderer
+const defaultColumn = {
+  cell: (info) => EditableCell(info),
+};
+
 /* eslint-disable react/jsx-props-no-spreading */
 function Table({
   columns, data: defaultData,
@@ -22,6 +299,9 @@ function Table({
   const [data, setData] = React.useState(() => [...defaultData]);
   const [sorting, setSorting] = React.useState([]);
   const [globalFilter, setGlobalFilter] = React.useState('');
+  const [editing, setEditing] = React.useState(isEditing);
+
+  const cellToFocus = React.useRef({ index: null, id: null });
 
   const columnVisibility = {};
   if (!isEditing) {
@@ -58,12 +338,20 @@ function Table({
   const table = useReactTable({
     data,
     columns,
+    defaultColumn,
     initialState: {
       columnVisibility,
     },
     state: {
       sorting,
       globalFilter,
+    },
+    meta: {
+      isEditing,
+      limited,
+      useScaling,
+      onConnectionChange,
+      cellToFocus,
     },
     onSortingChange: setSorting,
     onGlobalFilterChange: setGlobalFilter,
@@ -185,63 +473,51 @@ function VariablesEditor(props) {
   const columns = [
     columnHelper.accessor('active', {
       header: () => '#',
-      cell: (info) => info.getValue(),
+      cell: (info) => CheckButtonCell(info),
     }),
     columnHelper.accessor('from', {
       header: () => 'From',
-      cell: (info) => info.getValue(), // ReadonlyCell
+      cell: (info) => ReadonlyCell(info),
     }),
     columnHelper.accessor('name', {
       header: () => 'Name',
-      cell: (info) => info.getValue(),
     }),
     columnHelper.accessor('role', {
       header: () => 'Role',
-      cell: (info) => info.getValue(),
     }),
     columnHelper.accessor('desc', {
       header: () => 'Description',
-      cell: (info) => info.getValue(),
     }),
     columnHelper.accessor('type', {
       header: () => 'Type',
-      cell: (info) => info.getValue(),
     }),
     columnHelper.accessor('shape', {
       header: () => 'Shape',
-      cell: (info) => info.getValue(),
     }),
     columnHelper.accessor('units', {
       header: () => 'Units',
-      cell: (info) => info.getValue(),
     }),
     columnHelper.accessor('init', {
       header: () => 'Init',
-      cell: (info) => info.getValue(),
     }),
     columnHelper.accessor('lower', {
       header: () => 'Lower',
-      cell: (info) => info.getValue(),
     }),
     columnHelper.accessor('upper', {
       header: () => 'Upper',
-      cell: (info) => info.getValue(),
     }),
     columnHelper.accessor('uq', {
       header: () => 'UQ',
-      cell: (info) => '',
+      cell: (info) => ButtonCell(info),
     }),
     columnHelper.accessor('ref', {
       header: () => 'Ref',
-      cell: (info) => info.getValue(),
     }),
     columnHelper.accessor('ref0', {
       header: () => 'Ref0',
-      cell: (info) => info.getValue(),
     }),
     columnHelper.accessor('res_ref', {
       header: () => 'Res.Ref',
-      cell: (info) => info.getValue(),
     }),
   ];
 
