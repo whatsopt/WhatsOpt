86602d1f9247b448b1398a85f7c170df6d328c7f
relf <remi.lafage@onera.fr>
2023-04-17 14:59:54 +0200

    Specify integer for reference in package migration

--- db/migrate/20230403155358_create_packages.rb
+++ db/migrate/20230403155358_create_packages.rb
@@ -2,8 +2,8 @@ class CreatePackages < ActiveRecord::Migration[7.0]
   def change
     create_table :packages do |t|
       t.text :description
-      t.references :analysis, foreign_key: true
-
+      t.references :analysis, foreign_key: true, type: :integer
+      
       t.timestamps
     end
   end
