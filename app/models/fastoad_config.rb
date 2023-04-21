eef2495575ac81be68c54bab342caf45683364ae
relf <remi.lafage@onera.fr>
2023-04-21 15:24:30 +0200

    Manage base config fastoad 1.4.1

--- app/models/fastoad_config.rb
+++ app/models/fastoad_config.rb
@@ -9,14 +9,34 @@ class FastoadConfig < ApplicationRecord
   DEFAULT_OUTPUT_FILE = "./output_file.xml"
 
   belongs_to :analysis
-  has_many :modules, class_name: "FastoadModule"
+  has_many :fastoad_modules
 
   validates :name, presence: true, uniqueness: true
+  validates :version, presence: true
   validates :module_folders, presence: true
   validates :input_file, presence: true
   validates :output_file, presence: true
 
   after_initialize :set_defaults
+
+  def load_conf(version)
+    filepath = File.join(Rails.root, "config", "fastoad", "fastoad_config_1.4.1.yml")
+    conf = YAML.load(File.open(filepath).read)
+    conf
+  end
+
+  def list_modules(model)
+    modules = []
+    model.each do |k, mod|
+      next if ["linear_solver", "nonlinear_solver"].include?(k)
+      if mod['id']
+        modules << FastoadModule.new(name: k, version: "1.4.1", fastoad_id: mod['id'])
+      else
+        modules += list_modules(mod)
+      end
+    end
+    modules
+  end
 
 private
 
@@ -24,6 +44,17 @@ private
     self.module_folders = DEFAULT_MODULE_FOLDERS if module_folders.blank?
     self.input_file = DEFAULT_INPUT_FILE if input_file.blank?
     self.output_file  = DEFAULT_OUTPUT_FILE if output_file.blank?
+
+    if fastoad_modules.blank?
+      conf = self.load_conf(self.version)
+
+      self.module_folders = conf['module_folders']
+      self.input_file = conf['input_file']
+      self.output_file = conf['output_file']
+
+      modules = list_modules(conf['model'])
+      self.fastoad_modules = modules
+    end
   end
 
 end
