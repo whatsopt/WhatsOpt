754c3f7f6022698d31cd3da8a7e5f15627c00c7d
Rémi Lafage <remi.lafage@onera.fr>
2023-07-05 14:16:07 +0200

    Use relevant data test for PCE (#174)

--- services/tests/test_openturns_surrogates.py
+++ services/tests/test_openturns_surrogates.py
@@ -3,11 +3,21 @@ import numpy as np
 from whatsopt_server.surrogate_store.openturns_surrogates import PCE
 
 
+import numpy as np
+
+
+def xsinx(x):
+    x = np.reshape(x, (-1,))
+    y = np.zeros(x.shape)
+    y = (x - 3.5) * np.sin((x - 3.5) / (np.pi))
+    return y.reshape((-1, 1))
+
+
 class TestOpenturnsSurrogates(unittest.TestCase):
     def setUp(self):
-        xt = np.array([[0.0, 1.0, 2.0, 3.0, 4.0]]).T
-        yt = np.array([0.0, 1.0, 1.5, 0.5, 1.0]).T
         self.surr = PCE()
+        xt = np.atleast_2d(np.random.uniform(0, 25, 200)).T
+        yt = xsinx(xt)
         self.surr.set_training_values(xt, yt)
 
     def tearDown(self):
@@ -17,11 +27,9 @@ class TestOpenturnsSurrogates(unittest.TestCase):
         self.surr.train()
 
         num = 13
-        x = np.linspace(0.0, 4.0, num).reshape(-1, 1)
+        x = np.linspace(0.0, 25.0, num).reshape(-1, 1)
         y = self.surr.predict_values(x)
-
         self.assertEqual((13, 1), y.shape)
-
         sa = self.surr.get_sobol_indices()
         self.assertEqual(1.0, sa.getSobolIndex(0))
 
