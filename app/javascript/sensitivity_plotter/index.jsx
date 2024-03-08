659f62943b6bb7fe3da6160e49a10914830d6ab5
relf <remi.lafage@onera.fr>
2024-03-08 16:39:00 +0100

    Hotfix: js sensitivity api has changed with HSIC prototyping, to be refactred

--- app/javascript/sensitivity_plotter/index.jsx
+++ app/javascript/sensitivity_plotter/index.jsx
@@ -16,6 +16,9 @@ class SensitivityPlotter extends React.Component {
     const { api, ope: { id: opeId } } = this.props;
     api.analyseSensitivity(
       opeId,
+      undefined, // FIXME: HSIC parameters, unused here, to be refactored
+      undefined, // FIXME: HSIC parameters, unused here, to be refactored
+      undefined, // FIXME: HSIC parameters, unused here, to be refactored
       (response) => {
         this.setState({ ...response.data });
       },
