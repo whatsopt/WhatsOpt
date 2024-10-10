b952d6d0eb2443bc60b3e47671d149ef6e1902db
relf <remi.lafage@onera.fr>
2024-10-10 11:36:31 +0200

    deploy with ruby 3.3.5

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,7 +5,7 @@ set :stage, :staging
 set :server, ENV["WHATSOPT_STAGING_SERVER"]
 
 set :deploy_to, "#{ENV['WHATSOPT_DEPLOY_DIR']}"
-set :rvm_ruby_version, "ruby-3.3.0@whatsopt"
+set :rvm_ruby_version, "ruby-3.3.5@whatsopt"
 server fetch(:server), user: "#{ENV['WHATSOPT_DEPLOY_USER']}", roles: %w{app web db}, primary: true
 
 set :passenger_restart_with_touch, true
