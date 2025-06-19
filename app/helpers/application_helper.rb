7f31ba8cd46ca584ed7ea984117125a53d4aa0cd
relf <remi.lafage@onera.fr>
2025-06-19 18:31:00 +0200

    Fix logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
     else
       "logo_whatsopt_yellow_v1.0.svg"
     end
-    image_tag("#{}", alt: "Logo", width: 32, height: 32, class: "d-inline-block align-top")
+    image_tag("#{image_svg}", alt: "Logo", width: 32, height: 32, class: "d-inline-block align-top")
   end
 
   def bootstrap_class_for(flash_type)
